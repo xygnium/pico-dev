911a62be2052235006e5481009c923e96d8ca314
dev0 <xygnium@gmail.com>
2025-07-04 10:26:04 -0400

    add scsd repo

---
+++ scsd/load_scsd.sh
@@ -0,0 +1,14 @@
+#~/bin/bash
+
+#PRG=$PICO_DEV/pico-examples/build/blink/blink.elf
+#PRG=$PICO_DEV/gmcount/build/gmcount.elf
+#PRG=$HOME/dev/github/pico-dev/gmcount/build/gmcount.elf
+PRG=$HOME/dev/github/pico-dev/scsd/build/simple_example.elf
+
+PICO_DEV=/home/mike/dev/pico
+OPENOCD_D=$PICO_DEV/openocd
+OPENOCD=$OPENOCD_D/src/openocd
+IF_CFG=$OPENOCD_D/tcl/interface/cmsis-dap.cfg
+TGT_CFG=$OPENOCD_D/tcl/target/rp2040.cfg
+
+sudo $OPENOCD -s tcl -f $IF_CFG -f $TGT_CFG -c "adapter speed 5000" -c "program $PRG verify reset exit"
