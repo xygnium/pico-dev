62f50d55c2f606422aee20c7723f49b494792f3d
dev0 <xygnium@gmail.com>
2026-01-15 16:07:14 -0500

    add basic udp to app

--- wifi/main.c
+++ wifi/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
+#include "lwip/pbuf.h"
+#include "lwip/udp.h"
 
 int setup(uint32_t country, const char *ssid, const char *pass,  uint32_t auth)
 {
@@ -24,6 +26,22 @@ int setup(uint32_t country, const char *ssid, const char *pass,  uint32_t auth)
   return 0;
 }
 
+#define BUF_SIZE 1024
+
+void recv(void *arg, struct udp_pcb *pcb, struct pbuf *p, const ip_addr_t *addr, u16_t port)
+{
+    char myBuff[BUF_SIZE];
+    if (p != NULL)
+    {
+        printf("recv total %d  this buffer %d next %d \n", p->tot_len, p->len, p->next);
+        printf("From %s:%d\n", ipaddr_ntoa(addr), port);
+        pbuf_copy_partial(p, myBuff, p->tot_len, 0);
+        myBuff[p->tot_len] = 0;
+        printf("Buffer= %s\n", myBuff);
+        pbuf_free(p);
+    }
+}
+
 char ssid[] = "abzu2";
 char pass[] = "keyboardPlank20#";
 uint32_t country = CYW43_COUNTRY_USA;
@@ -35,6 +53,9 @@ int main()
     printf("init stdio complete\n");
     setup(country, ssid, pass, auth);
     printf("forever loop\n");
+    struct udp_pcb *pcb = udp_new();
+    udp_recv(pcb, recv, NULL);
+    udp_bind(pcb, IP_ADDR_ANY, 8080);
     while (true)
     {
         sleep_ms(1);
--- wifi/udp_client.py
+++ wifi/udp_client.py
@@ -2,14 +2,18 @@
 
 import socket
 
-HOST="127.0.0.1"
-PORT=9997
-BUFSIZE=2048
+#HOST="127.0.0.1"
+HOST="192.168.1.121"
+PORT=8080
+BUFSIZE=1024
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-inString = input()
-sock.sendto(inString.encode('utf-8'), (HOST, PORT))
-payload, addr = sock.recvfrom(BUFSIZE)
-print(payload.decode())
+print("Enter command:")
+cmd = input()
+print("cmd=", cmd)
+sock.sendto(cmd.encode('ascii'), (HOST, PORT))
+#sock.sendto(inString.encode('utf-8'), (HOST, PORT))
+#payload, addr = sock.recvfrom(BUFSIZE)
+#print(payload.decode())
 sock.close()
